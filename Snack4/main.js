889f0cec636e07ea833bf7011014d7c7fd06ac42
AndreaPanzeriDev <andpanz@libero.it>
2022-10-26 14:46:28 +0200

    commenti

--- Snack4/main.js
+++ Snack4/main.js
@@ -4,18 +4,22 @@ Interrompi il ciclo appena il nome è stato trovato.
 */
 
 
-
+//list guest
 const guest = ["andrea" , "marco" , "matteo" , "sofia", "alessandro", "Elsti"]
 
+
+//ask the name of the guest
 let nameGuest = prompt("Inserisci il tuo nome: ");
 
+//variable
 let i=0;
 
 while(true){
-
+    //check if the name is into the guest's list
     if(nameGuest == guest[i]){
         console.log("prego può entrare");
         break;
+    //if you finished the list you're not allowed to enter
     }else if(i = (guest.length - 1)){
         console.log("il suo nome non è sulla lista");
         break;

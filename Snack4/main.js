f4c7353d1dca2af8dc9c7967f886293cc5a52f1d
AndreaPanzeriDev <andpanz@libero.it>
2022-10-26 14:41:21 +0200

    Snack4

---
+++ Snack4/main.js
@@ -0,0 +1,26 @@
+/*
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+Interrompi il ciclo appena il nome è stato trovato.
+*/
+
+
+
+const guest = ["andrea" , "marco" , "matteo" , "sofia", "alessandro", "Elsti"]
+
+let nameGuest = prompt("Inserisci il tuo nome: ");
+
+let i=0;
+
+while(true){
+
+    if(nameGuest == guest[i]){
+        console.log("prego può entrare");
+        break;
+    }else if(i = (guest.length - 1)){
+        console.log("il suo nome non è sulla lista");
+        break;
+    }
+    i++;
+}
+
+

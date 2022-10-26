55c903809a2329158514bd404224f42850ee9af4
AndreaPanzeriDev <andpanz@libero.it>
2022-10-26 13:07:31 +0200

    Snack2

---
+++ Snack2/main.js
@@ -0,0 +1,17 @@
+/*
+il software deve chiedere per 5 volte all'utente di inserire un numero.
+il programma stampa la somma di tutti i numeri inseriti
+*/
+
+let x = 0;
+
+let sum = 0;
+
+while(x<5){
+
+    let number = parseInt(prompt("Inserire un numero"));
+    sum += number;
+    x++;
+}
+
+console.log("La somma dei numeri inseriti è: " , sum);
